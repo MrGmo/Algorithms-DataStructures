90bbc881e376bc40c6347cf44f81d7ff5ec645ec
Guillermo Aguilar <gaguilar128@gmail.com>
2021-03-21 11:03:39 -0500

    Algorithm Practice

--- problemSolvingPractice.js
+++ problemSolvingPractice.js
@@ -1,5 +1,94 @@
 // Bonus Challenges
 
+
+// Sliding window
+// Write a function called findLongestSubstring, which accepts a string and returns the length fo the longest substring with all distinct characters.
+
+// function findLongestSubstring(str){
+//   let longest = 0;
+//   let seen = {};
+//   let start = 0;
+//
+//   for(let i = 0; i < str.length; i++){
+//     let char = str[i]
+//
+//     if(seen[char]){
+//       start = Math.max(start, seen[char]);
+//     }
+//     longest = Math.max(longest, i - start + 1);
+//     seen[char] = i + 1;
+//   }
+//   return longest;
+// }
+//
+//
+// console.log(findLongestSubstring('rithmschool'))
+// should return 7
+
+
+// Sliding window - really tough one :(
+// Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer. This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+
+
+// function minSubArrayLen(nums, sum) {
+//   let total = 0;
+//   let start = 0;
+//   let end = 0;
+//   let minLen = Infinity;
+//
+//   while (start < nums.length) {
+//     // if current window doesn't add up to the given sum then
+// 		// move the window to right
+//     if(total < sum && end < nums.length){
+//       total += nums[end];
+// 			end++;
+//     }
+//     // if current window adds up to at least the sum given then
+// 		// we can shrink the window
+//     else if(total >= sum){
+//       minLen = Math.min(minLen, end-start);
+// 			total -= nums[start];
+// 			start++;
+//     }
+//     // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+//     else {
+//       break;
+//     }
+//   }
+//
+//   return minLen === Infinity ? 0 : minLen;
+// }
+//
+// console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7))
+
+
+// Sliding window
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed ot the function. The elements must be consecutive.
+
+// function maxSubarraySum(arr, num){
+//     let maxSum = 0
+//     let tempSum = 0
+//
+//     if(num > arr.length){
+//       return null;
+//     }
+//
+//     for(let i = 0; i < num; i++){
+//       maxSum += arr[i]
+//     }
+//
+//     tempSum = maxSum
+//     for(let j = num; j < arr.length; j++){
+//       tempSum = tempSum - arr[j - num] + arr[j]
+//       maxSum = Math.max(maxSum, tempSum)
+//     }
+//       return maxSum
+//   }
+
+// console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4))
+// should return 39
+
+
 //Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
 
 // Using a recursive solution
