a5af00d0af101ea44bc222602b0608938ea930a9
Guillermo Aguilar <gaguilar128@gmail.com>
2021-03-27 14:13:10 -0500

    Algorithms Practice

--- problemSolvingPractice.js
+++ problemSolvingPractice.js
@@ -1,5 +1,16 @@
 // Bonus Challenges
 
+// Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+
+
+// function fib(num){
+//   if(num === 1 || num === 2) return 1;
+//   return fib(num - 1) + fib(num - 2)
+// }
+//
+// console.log(fib(10))
+// should return 55
+
 
 //Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function.
 
