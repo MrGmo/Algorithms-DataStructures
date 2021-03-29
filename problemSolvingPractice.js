d95b1d3663d27d125382071658513ac796184baf
Guillermo Aguilar <gaguilar128@gmail.com>
2021-03-29 13:57:32 -0500

    Algo Practice

--- problemSolvingPractice.js
+++ problemSolvingPractice.js
@@ -1,5 +1,16 @@
 // Bonus Challenges
 
+//Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
+
+// function isPalindrome(str){
+//   if(str.length === 1) return true;
+//   if(str.length === 2) return str[0] === str[1];
+//   if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1))
+//   return false;
+// }
+// console.log(isPalindrome('racecar'))
+
+
 //Write a recursive function called reverse which accepts a string and returns a new string in reverse.
 
 // function reverse(str){
