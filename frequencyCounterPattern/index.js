534d7f2c841f988eb8882e9f7eef520bca50f8bc
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-16 03:05:37 -0500

    Algo Practice

---
+++ frequencyCounterPattern/index.js
@@ -0,0 +1,72 @@
+// Frequency Counter Example 1
+// Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+
+// Solution 1
+// function same(arr1, arr2){
+//   if(arr1.length !== arr2.length){
+//     return false
+//   }
+
+//   for(let i = 0; i < arr1.length; i++){
+//     let correctIndex = arr2.indexOf(arr1[i]**2)
+//     if(correctIndex === -1){
+//       return false
+//     }
+//     arr2.splice(correctIndex, 1)
+//   }
+//   return true
+// }
+
+// Solution 2
+function same(arr1, arr2){
+  
+}
+
+
+
+console.log(same([1, 2, 3], [1, 4, 9]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+
+
+// Use the multiple pointer method
+// Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+
+
+// Find the unique number in the array using the frequency counter method.
+
+
+
+// Frequency Counter Example 2
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another. Such as cinema, formed from iceman.
+
+
+
+
+
+
+
+
+// Write a function which take in a string and returns counts of each character in the string.
+
+
+
+
+// Write a function that calculates the sum of all nubmers from 1 up to (and including) some number n.
+
+
+
+
