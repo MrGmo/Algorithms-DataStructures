97513063991f5b2fa8833dd600fd139900b41476
Guillermo Aguilar <gaguilar128@gmail.com>
2021-07-19 16:39:06 -0500

    Algo practice

--- insertionSort/index.js
+++ insertionSort/index.js
@@ -3,14 +3,14 @@
 
 // Solution 1 - Time: O(n^2)
 
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let currentValue = arr[i]
-    for (let j = i-1; j >= 0 && arr[j] > currentValue; j--) {
-      if (arr[j] > currentValue) {
+function insertionSort(arr){
+	let currentVal;
+    for(let i = 1; i < arr.length; i++){
+      currentVal = arr[i]
+      for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
         arr[j+1] = arr[j]
       }
-    }
+      arr[j+1] = currentVal
   }
-  return arr
+    return arr
 }
