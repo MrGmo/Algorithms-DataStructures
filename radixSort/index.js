c3eb17c8de7a46bc15bcae5c36a1a81c702681e4
Guillermo Aguilar <gaguilar128@gmail.com>
2021-07-21 16:38:09 -0500

    Algo practice

--- radixSort/index.js
+++ radixSort/index.js
@@ -1,5 +1,34 @@
 //Radix Sort Example 1
 //Write a function that takes in an array and sorts it smallest to largest using radix sort.
 
-// Solution 1 - Time: O(n^2), Space: O(log(n))
+// Solution 1 - Time: O(nk), Space: O(n+k)
 
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+function radixSort(nums){
+  let maxDigitCount = mostDigits(nums);
+  for(let k = 0; k < maxDigitCount; k++){
+    let digitBuckets = Array.from({length: 10}, () => []);
+    for(let i = 0; i < nums.length; i++){
+      let digit = getDigit(nums[i],k);
+      digitBuckets[digit].push(nums[i]);
+  }
+    nums = [].concat(...digitBuckets);
+  }
+    return nums;
+}
