fdb6e9dad378cd4445f9ed33a7f6a814e6ce4c8d
Guillermo Aguilar <gaguilar128@gmail.com>
2021-07-23 19:48:01 -0500

    Algo practice

--- doublyLinkedLists/index.js
+++ doublyLinkedLists/index.js
@@ -20,9 +20,9 @@ class DoublyLinkedList() {
       this.head = newNode
       this.tail = newNode
     } else {
-    this.tail.next = newNode
-    newNode.prev = this.tail
-    this.tail = newNode
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
     }
     this.length++
     return this
@@ -47,11 +47,81 @@ class DoublyLinkedList() {
     if (this.length === 1) {
       this.head = null
       this.tail = null
+    } else {
+      this.head = oldHead.next
+      this.head.prev = null
+      oldHead.next = null
     }
-    this.head = oldHead.next
-    this.head.prev = null
-    
+    this.length--
+    return oldHead
+  }
+  unshift(val) {
+    let newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.head.prev = newNode
+      newNode.next = this.head
+      this.head = newNode
+    }
+    this.length++
+    return this
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index <= this.length/2) {
+      let count = 0
+      let current = this.head
+      while (count !== index) {
+        current = current.next
+        count++
+      }
+    } else {
+      let count = this.length-1
+      let current = this.tail
+      while (count !== index) {
+        current = current.prev
+        count--
+      }
+    }
+    return current
+  }
+  set(index,val) {
+    //set is much easier if you already have a get method, FYI 
+    let foundNode = this.get(index)
+    if (foundNode !== null) {
+      foundNode.val = val
+      return true
+    }
+    return false
+  }
+  insert(index,val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+    let newNode = new Node(val);
+    let beforeNode = this.get(index-1);
+    let afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++
+    return true
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length-1) return this.pop();
+    let removedNode = this.get(index)
+    removedNode.prev.next = removedNode.next
+    removedNode.next.prev = removedNode.prev
+    removedNode.next = null
+    removedNode.prev = null
+    this.length--
+    return removedNode
   }
 }
 
-
+let list = new DoublyLinkedList()
