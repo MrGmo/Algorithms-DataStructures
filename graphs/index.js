d3795aaa6292198cc98208493c57c1833c966da8
Guillermo Aguilar <gaguilar128@gmail.com>
2021-07-28 15:04:54 -0500

    Algo practice

--- graphs/index.js
+++ graphs/index.js
@@ -11,6 +11,17 @@ class Graph {
     this.adjacencyList[v1].push[v2]
     this.adjacencyList[v2].push[v1]
   }
+  removeEdge(vertex1,vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2)
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      let adjacentVertex = this.adjacencyList[vertex].pop()
+      this.removeEdge(vertex, adjacentVertex)
+    }
+    delete this.adjacencyList[vertex]
+  }
 }
 
 let g = new Graph()
